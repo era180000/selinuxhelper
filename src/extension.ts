e6580ed0aab7cfd2e6d3f715236488da910c3816
era180000 <92063174+era180000@users.noreply.github.com>
2023-03-22 13:10:03 -0500

    Remove Default Commands

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('selinuxhelper.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SELinuxHelper!');
+		//vscode.window.showInformationMessage('Hello World from SELinuxHelper!');
 	});
 
 	context.subscriptions.push(disposable);
