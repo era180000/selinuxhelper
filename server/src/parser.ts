b78f5a56ea8286f36544c0cef95a2b56a5a47abf
era180000 <92063174+era180000@users.noreply.github.com>
2023-04-20 17:36:21 -0500

    remove Parse File
    first test of remove parse file. dunno if this function is even called yet
    updated parseIF to include templates

--- server/src/parser.ts
+++ server/src/parser.ts
@@ -37,9 +37,9 @@ export class FileParser {
    
     //parses a file and adds it to the definition table
     parseFile(uri: string) {
-        //if this document has been parsed 
+        //if this document has been already been parsed 
         if(this.documentList.indexOf(uri) !== -1){
-            this.removeFileParse(uri);
+            this.removeFileParse(uri); //remove file parse then reparse
         }
 
         const filePath = URI.parse(uri).fsPath;
@@ -67,7 +67,7 @@ export class FileParser {
 
         for(let i = 0; i < lines.length; i++)
         {
-            if (/^\s*interface\(/.test(lines[i])) { // check if line starts with "interface
+            if (/^\s*(interface|template)\(/.test(lines[i])) { // check if line starts with "interface
                 const match = lines[i].match(/(?<=`)(.*)(?=')/m);
                 if(match) {
                     //find end line of
@@ -150,17 +150,45 @@ export class FileParser {
     //TO-DO: There is something wrong with this function idk
     removeFileParse(uri: string){
 
+        //remove document from list
         delete this.documentList[this.documentList.indexOf(uri)];
+
         //go through the map
         //for each symbol
-            //if the value is a list
-            // iterate through list and remove any matches
-            //  if at the end the list size is 0 delete symbol from map
-            //  if at the end the list size is 1, replace it with symbol and just the first value
-            //if the value is a single locations
-            //  check if the location matches uri
-            //  if so, remove from map
-    
+        for (let [key, value] of this.definitionTable.entries()) {
+            if (Array.isArray(value)){ //if the value is a list
+                for (let element of value){ //iterate through list and remove any matches
+                    if (element.uri === uri){
+                        //remove element from list
+                        //this will remove the element and not set the index to undefined
+                        const index = value.indexOf(element);
+                        if (index > -1) {
+                            value.splice(index, 1); //syntax splice(index, #of elements to remove at location)
+                        }
+                    }
+                }
+                // if at the end the list size is 0 delete symbol from map
+                if (value.length == 0){
+                    this.definitionTable.delete(key);
+                }
+                //if at the end the list size is 1, replace it with symbol and just the first value
+                else if (value.length == 1){
+                    const singleLocation = value[0];
+                    const singleKey = key;
+                    this.definitionTable.delete(key);
+                    this.definitionTable.set(singleKey, singleLocation);
+                }
+            }
+            //  if the value is a single locations
+            else{
+                //check if the location matches uri
+                if(value.uri === uri){
+                    //if so, remove from map
+                    this.definitionTable.delete(key);
+                }
+            }
+        }
+
     }
 
     //returns definition array for a symbol
